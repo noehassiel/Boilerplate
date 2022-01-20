d2f31aa08df1493c33bf7bd38a596974651b1c25
noehassiel <artetools@iMac-de-Arte.local>
2022-01-20 15:19:09 -0600

    c

--- webpack.config.js
+++ webpack.config.js
@@ -8,8 +8,10 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const IS_DEVELOPMENT = process.env.NODE_ENV === 'dev'
 
 const dirApp = path.join(__dirname, 'app')
+const dirImages = path.join(__dirname, 'images')
 const dirShared = path.join(__dirname, 'shared')
 const dirStyles = path.join(__dirname, 'styles')
+const dirVideos = path.join(__dirname, 'videos')
 const dirNode = 'node_modules'
 
 module.exports = {
@@ -20,10 +22,13 @@ module.exports = {
 
     resolve: {
         modules: [
-            dirApp, 
+            dirApp,
+            dirImages, 
             dirShared, 
             dirStyles, 
-            dirNode],
+            dirVideos,
+            dirNode,
+        ],
       },
 
     plugins: [
@@ -38,6 +43,65 @@ module.exports = {
                 to: '',
               },
             ],
-          })
-    ]
+          }),
+
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css'
+          })  
+    ],
+
+    module:{
+        rules:[
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: ''
+                        }
+                    },
+                    {
+                        loader: 'css-loader',
+                    },
+                    {
+                        loader: 'postcss-loader',
+                    },
+                    {
+                        loader: 'sass-loader',
+                    }
+                ]
+            },
+
+            {
+                test: /\.(jpe?g|png|gif|svg|woff2?|fnt|webp)$/,
+                loader: 'file-loader',
+                options: {
+                    name (file) {
+                        return '[hash].[ext]'
+                    }
+                }
+            },
+
+            {
+                test: /\.(glsl|frag|vert)$/,
+                loader: 'raw-loader',
+                exclude: /node_modules/
+              },
+        
+              {
+                test: /\.(glsl|frag|vert)$/,
+                loader: 'glslify-loader',
+                exclude: /node_modules/
+              }
+        ]
+    }
 }
